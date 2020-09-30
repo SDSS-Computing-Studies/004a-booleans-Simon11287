265d2fbaf2bfe03e09061bf86df63abfc5cecc67
Simon11287 <71406841+Simon11287@users.noreply.github.com>
2020-09-30 13:01:08 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -10,3 +10,24 @@
 # - "positive"
 # - "negative"
 # - "zero"
+
+print("=============================")
+print("Determine if the number is positive, negative or 0  ")
+print("=============================")
+A = float(input("pick a number: "))
+B = 0
+
+positive = True
+negative =  True
+zero = True
+
+if A > B :
+  print("The number is a positive")
+
+elif A < B :
+  print("The number is a negative")
+
+else :
+  print("The number is a 0")
+  
+print("=============================\n\n\n")
