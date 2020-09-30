4e9ebdb86b41ce9c9e5fb85af5e29ebdf08dcec6
Simon11287 <71406841+Simon11287@users.noreply.github.com>
2020-09-30 11:50:08 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -11,3 +11,25 @@
 # "The number is larger than 100"
 # "The number is smaller than 100"
 # "The number is 100"
+
+a = float(input("pick a number: "))
+
+print("=============================")
+print("Determine if the number is larger than 100 ")
+print("=============================")
+
+b = 100
+Equal to 100 = True
+Over 100 = True
+Under 100 = False
+a==b
+if Over:
+  print("The number is larger than 100")
+
+if Under:
+  print("The number is smaller than 100")
+
+if Equal:
+  print("The number is 100")
+  
+print("=============================\n\n\n")
